fe4544e966f13304afdff03fe8c5cc95d586e9f6
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-25 12:44:31 +0100

    notes added on using class based views

--- BooklistAPI/views.py
+++ BooklistAPI/views.py
@@ -4,7 +4,22 @@ from rest_framework import status
 # from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
-@api_view()
+@api_view(['GET', 'POST'])
 #pass in methods in an array into the api_view function
 def books(request):
     return Response('list of the books', status=status.HTTP_200_OK)
+
+
+#you can map a method from a class, you need to declare that method as a static method
+
+
+class Orders():
+    @staticmethod
+    @api_view()
+
+    def listOrders(request):
+        return Response({'message': 'list of orders'}, 200)
+    
+
+#Routing class-based views
+
