a69c37a8d4a4c2a8394e023ffebfccda1e0715be
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-25 12:28:58 +0100

    added comments to view clas

--- BooklistAPI/views.py
+++ BooklistAPI/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
+# from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
-
 @api_view()
+#pass in methods in an array into the api_view function
 def books(request):
     return Response('list of the books', status=status.HTTP_200_OK)
