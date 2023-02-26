31c043054f6bb890f2a1c6c5b0093631ebfe9108
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-26 13:24:57 +0100

    typo error fixed

--- BooklistAPI/views.py
+++ BooklistAPI/views.py
@@ -43,6 +43,6 @@ class BookView(viewsets.ViewSet):
 
 class BookViews(APIView):
     def get(self, request, pk):
-        return Response({"message": "single book with id" + str(pk)}, status.HTTP_200_OK)
+        return Response({"message": "single book with id" + ' ' + str(pk)}, status.HTTP_200_OK)
     def put(self, request, pk):
         return Response({"title": request.data.get('title')}, status.HTTP_200_OK)
