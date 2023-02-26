6993d82c415b499177552e5d629d0dffc23611e6
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-26 13:23:21 +0100

    BookView updated

--- BooklistAPI/urls.py
+++ BooklistAPI/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     # path('books/', views.books, name="books"),
     path('orders', views.Orders.listOrders, name="orders"),
-    path('books', views.BookView.as_view({
-        'get': 'list',
-        'post': 'create',
-    }), name="books"),
-    path('books/<int:pk>', views.BookView.as_view, name="bookview")
+    # path('books', views.BookView.as_view({
+    #     'get': 'list',
+    #     'post': 'create',
+    # }), name="books"),
+    path('books/<int:pk>', views.BookViews.as_view())
 ]
